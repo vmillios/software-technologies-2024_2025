28fe6fa2f701b7597b07a6738f3466105c860d38
Vasileios Millios <billmillios1@gmail.com>
2025-04-15 02:12:07 +0300

    used dataframe for showing data

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import scanpy as sc
+import pandas as pd
+
 
 def print_data(adata: sc.AnnData) -> None:
-    st.write(f"n_obs: {adata.n_obs}")
-    st.write(f"n_vars: {adata.n_vars}")
-    st.write(f"obs:  {", ".join(adata.obs_keys())}")
+    df = pd.DataFrame(
+        {
+            "atribute": ["n_obs", "n_vars", "obs"],
+            "data": [adata.n_obs, adata.n_vars, ", ".join(adata.obs_keys())]
+        }
+    )
+    st.dataframe(df)
+    # st.write(f"n_obs: {adata.n_obs}")
+    # st.write(f"n_vars: {adata.n_vars}")
+    # st.write(f"obs:  {", ".join(adata.obs_keys())}")
