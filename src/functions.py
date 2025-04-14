32a8ccf73e99fa91300ff03ccd3bd82d56fca8d9
Vasileios Millios <billmillios1@gmail.com>
2025-04-15 02:03:41 +0300

    updated showing data after uploading

--- src/functions.py
+++ src/functions.py
@@ -2,4 +2,6 @@ import streamlit as st
 import scanpy as sc
 
 def print_data(adata: sc.AnnData) -> None:
-    st.write(f"obs: {adata.obs_names}")
+    st.write(f"n_obs: {adata.n_obs}")
+    st.write(f"n_vars: {adata.n_vars}")
+    st.write(f"obs:  {", ".join(adata.obs_keys())}")
