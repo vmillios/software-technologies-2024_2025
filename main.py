44cd41192671e0c02a773abedbc912a50ef04f57
Soupioni Margarita <soupionimargarita09@gmail.com>
2025-04-15 01:27:24 +0300

    addded the upload file function

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ import streamlit as st
 with open("templates/main.md", encoding="utf-8") as f:
     template = f.read()
 
-if __name__ == "__main__":
+def main():
     st.write(template)
+    uploaded_file = st.file_uploader("Upload a h5ad file", type=["h5ad"])
+    if uploaded_file:
+        st.write("file has been uploaded")
+
+if __name__ == "__main__":
+    main()
