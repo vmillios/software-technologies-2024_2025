df6ed7bc92bd6857ae4b88a9e35116c0b8094a38
Vasileios Millios <billmillios1@gmail.com>
2025-04-15 03:51:50 +0300

    added preprocessing functions and plots

--- main.py
+++ main.py
@@ -18,13 +18,23 @@ def main() -> None:
         try:
             adata = sc.read(tmp_path)
             st.success("File uploaded successfully!")
-            src.functions.print_data(adata)
-            min_gen_value =st.number_input ("min_gen_value",value=None)
-            min_disp_value =st.number_input ("max_gen_value",value=None)
-            min_cells_value =st.number_input ("min_cells_value",value=None)
-            target_sum_value =st.number_input ("target_sum_value",value=None)
-            min_mean_value =st.number_input ("min_mean_value",value=None)
-            max_mean_value =st.number_input ("max_mean_value",value=None)
+            src.functions.print_data_preprocessing(adata)
+
+            st.header("preprocessing parameters")
+            min_genes_value =st.number_input ("min_genes_value",value=600)
+            min_cells_value =st.number_input ("min_cells_value",value=3)
+            target_sum_value =st.number_input ("target_sum_value",value=1e4)
+            min_mean_value =st.number_input ("min_mean_value",value=0.0125)
+            max_mean_value =st.number_input ("max_mean_value",value=3)
+            min_disp_value =st.number_input ("min_disp_value",value=0.5)
+            if st.button("start preprocessing"):
+                adata = src.functions.preprocess(adata, min_genes_value, min_cells_value, target_sum_value, min_mean_value, max_mean_value, min_disp_value)
+                src.functions.print_data_postprocessing(adata)
+                st.session_state["adata"] = adata
+            if "adata" in st.session_state:
+                src.functions.choose_plot(st.session_state["adata"])
+            
+
 
 
         except Exception as e:
--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import scanpy as sc
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def print_data(adata: sc.AnnData) -> None:
+
+def print_data_preprocessing(adata: sc.AnnData) -> None:
     df = pd.DataFrame(
         {
             "atribute": ["n_obs", "n_vars", "obs"],
@@ -11,6 +13,52 @@ def print_data(adata: sc.AnnData) -> None:
         }
     )
     st.dataframe(df)
-    # st.write(f"n_obs: {adata.n_obs}")
-    # st.write(f"n_vars: {adata.n_vars}")
-    # st.write(f"obs:  {", ".join(adata.obs_keys())}")
+
+def print_data_postprocessing(adata: sc.AnnData) -> None:
+    df = pd.DataFrame(
+        {
+            "atribute": ["obs", "var", "uns", "obsm", "varm", "obsp"],
+            "data": [", ".join(adata.obs_keys()), ", ".join(adata.var_keys()), ", ".join(adata.uns_keys()), ", ".join(adata.obsm_keys()), ", ".join(adata.varm_keys()), ", ".join(adata.obsp)]
+        }
+    )
+    st.dataframe(df)
+
+def preprocess(adata: sc.AnnData, min_genes_value: int, min_cells_value: int, target_sum_value: int, min_mean_value: float, max_mean_value: int, min_disp_value: float) -> sc.AnnData:
+    sc.pp.filter_cells(adata, min_genes=min_genes_value)
+    sc.pp.filter_genes(adata, min_cells=min_cells_value)
+    adata = adata[:, [gene for gene in adata.var_names if not str(gene).startswith(tuple(['ERCC', 'MT-', 'mt-']))]]
+    sc.pp.normalize_total(adata, target_sum=target_sum_value)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=min_mean_value, max_mean=max_mean_value, min_disp=min_disp_value)
+    adata.raw = adata
+    adata = adata[:, adata.var.highly_variable]
+    sc.pp.scale(adata, max_value=10)
+    sc.pp.pca(adata)
+    sc.pp.neighbors(adata)
+    sc.tl.umap(adata)
+    
+    return adata
+
+def choose_plot(adata : sc.AnnData):
+    option = st.selectbox(
+        "Select plot color",
+        ("batch", "celltype"),
+        index = None,
+        placeholder="plot color"
+    )
+    if option == "batch":
+        plot_show_batch(adata)
+    elif option == "celltype":
+        plot_show_celltype(adata)
+    else:
+        pass
+
+def plot_show_batch(adata: sc.AnnData) -> None:
+    fig, ax = plt.subplots(figsize=(6, 6))
+    sc.pl.umap(adata, color=['batch'], legend_fontsize=10, ax=ax, show=False)
+    st.pyplot(fig)
+
+def plot_show_celltype(adata: sc.AnnData) -> None:
+    fig, ax = plt.subplots(figsize=(6, 6))
+    sc.pl.umap(adata, color=['celltype'], legend_fontsize=10, ax=ax, show=False)
+    st.pyplot(fig)
